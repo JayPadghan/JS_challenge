3857e017a053442f31cf6c7619bc45f794c0f927
JayPadghan <padghanjay911@gmail.com>
2025-10-30 02:11:21 +0530

    scopes second part done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,3 +12,46 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "cartoon"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "cartoon"
+    if (username === "cartoon") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// **************** interesting *************
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
